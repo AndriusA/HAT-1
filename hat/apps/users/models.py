1b3eb564313c7b2d35fdeed3bba76cfcc29571aa
NabeelShaikh <nabeel.shaikh@live.com>
2015-02-19 20:45:03 +0000

    Reshuffled the table names in the order of table addition. Two tables added PersonAddressLocationCrossRef, PersonAddressRelationshipType

--- hat/apps/users/models.py
+++ hat/apps/users/models.py
@@ -166,28 +166,6 @@ class PersonAddress(CreateUpdateMixin, AddressMixin):
     class Meta:
         verbose_name_plural = u'People Addresses'
 
-class PersonAddressLocationCrossRef(CreateUpdateMixin):
-
-    #FK to Person Class
-    person_address_id = models.ForeignKey(
-        PersonAddress
-    )
-
-     #FK to DataDebit class
-    location_id = models.ForeignKey(
-        'locations.location'
-    )
-
-    relationship_type = models.ForeignKey(
-        'PersonAddressRelationshipType',
-        blank=True,
-        null=True,
-    )
-
-
-class PersonAddressRelationshipType(CreateUpdateMixin, NameDescMixin):
-    pass
-
 
 class PersonContactMethod(CreateUpdateMixin):
 
@@ -269,14 +247,58 @@ class PersonToPersonRelationshipType(CreateUpdateMixin, NameDescMixin):
         verbose_name_plural = u'P2P Relationship Types'
  
 
+class Emotion(CreateUpdateMixin, NameDescMixin):
 
-class DynamicType(CreateUpdateMixin, NameDescMixin):
+    #FK to Person
+    person = models.ForeignKey(
+        'Person'
+    )
 
-         #FK to Person
+    time = models.TimeField()
+
+    type = models.ForeignKey(
+        'EmotionType'
+    )
+
+    class Meta:
+        verbose_name_plural = u'Emotional States'
+
+
+class EmotionType(CreateUpdateMixin, NameDescMixin):
+    pass
+
+    class Meta:
+        verbose_name_plural = u'Emotional State Types'
+
+
+# Data about a person that may change - weight, blood pressure, heartrate etc.
+class PersonDynamic(CreateUpdateMixin):
+
+     #FK to Person
     person = models.ForeignKey(
         Person,
     )
 
+    type = models.ForeignKey(
+        'DynamicType'
+    )
+
+    value = models.CharField(
+        max_length=45
+    )
+
+    priority = models.PositiveIntegerField(
+        max_length=5,
+        blank=True,
+        null=True,
+    )
+
+    is_current = models.BooleanField(
+        default=False,
+    )
+
+
+class DynamicType(CreateUpdateMixin, NameDescMixin):
 
     sensor_data = models.ForeignKey(
         'things.SensorData'
@@ -321,4 +343,26 @@ class PersonDataDebitCrossRef(CreateUpdateMixin):
 
 
 class PersonDataDebitRelationshipType(CreateUpdateMixin, NameDescMixin):
+    pass
+
+class PersonAddressLocationCrossRef(CreateUpdateMixin):
+
+    #FK to Person Class
+    person_address_id = models.ForeignKey(
+        PersonAddress
+    )
+
+     #FK to DataDebit class
+    location_id = models.ForeignKey(
+        'locations.location'
+    )
+
+    relationship_type = models.ForeignKey(
+        'PersonAddressRelationshipType',
+        blank=True,
+        null=True,
+    )
+
+
+class PersonAddressRelationshipType(CreateUpdateMixin, NameDescMixin):
     pass
