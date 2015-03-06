f7afc3aa76799665658b51cb02961d9790ba8e67
mrmrtalbot <mrmrtalbot@googlemail.com>
2015-03-06 09:38:34 +0000

    DB changes
    Added Organisation Location + Organisation Sensor Cross Refs
    
    Removed Relationship Type Tables
    
    Added Relationship Type fields to Cross Ref tables

--- hat/apps/organisations/models.py
+++ hat/apps/organisations/models.py
@@ -44,6 +44,54 @@ class OrganisationAddress(CreateUpdateMixin, AddressMixin):
         verbose_name_plural = u'Organisation Addresses'
 
 
+class OrganisationLocationCrossRef(CreateUpdateMixin):
+    # LocationSensorCrossRef links the Location Class to the Sensor Class
+
+    # FK to Location class
+    location = models.ForeignKey(
+        'locations.Location',
+    )
+
+    # FK to Sensor class
+    Organisation = models.ForeignKey(
+        'Organisation',
+    )
+
+    common_name = models.CharField(
+        max_length=100,
+    )
+
+    relationship_type = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+    )
+
+
+class OrganisationSensorRef(CreateUpdateMixin):
+    # LocationSensorCrossRef links the Location Class to the Sensor Class
+
+    # FK to Location class
+    Organisation = models.ForeignKey(
+        'Organisation',
+    )
+
+    # FK to User class
+    Sensor = models.ForeignKey(
+        'things.Sensor',
+    )
+
+    common_name = models.CharField(
+        max_length=100,
+    )
+
+    relationship_type = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+    )
+
+
 class OrganisationContactMethod(CreateUpdateMixin):
 
     # FK to Organisation class
